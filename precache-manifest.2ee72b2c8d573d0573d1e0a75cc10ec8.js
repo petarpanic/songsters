ae8130ddee36edc0500cbb80ddebfca4cb5eb01f
Bosko <boshco.kuzmanovic@gmail.com>
2020-04-06 20:37:02 +0200

    Updates

---
+++ precache-manifest.2ee72b2c8d573d0573d1e0a75cc10ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259e56078d4dc0c7d230c1ba61427675",
+    "url": "/songsters/index.html"
+  },
+  {
+    "revision": "75915e002792818e090c",
+    "url": "/songsters/static/css/main.e2701b62.chunk.css"
+  },
+  {
+    "revision": "fda92436f49313280431",
+    "url": "/songsters/static/js/2.245adff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/songsters/static/js/2.245adff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75915e002792818e090c",
+    "url": "/songsters/static/js/main.cc0f3564.chunk.js"
+  },
+  {
+    "revision": "e4462dc50e36a9969696",
+    "url": "/songsters/static/js/runtime-main.f4cfbb4d.js"
+  }
+]);
